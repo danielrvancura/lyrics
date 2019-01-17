cb1e3af833b67d527a625bc4a4f0db3c3e092588
danielrvancura <danielvancura@gmail.com>
2019-01-16 16:11:37 -0800

    Updates

---
+++ precache-manifest.0ead502f70f98606e24595206f98b47a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "97727b84a6a986fd60a4d75865bf7c80",
+    "url": "/lyrics/static/media/spinner.97727b84.gif"
+  },
+  {
+    "revision": "9a6456428d717375188e",
+    "url": "/lyrics/static/js/runtime~main.9a645642.js"
+  },
+  {
+    "revision": "5d3ec2cf17e8c07a1ffe",
+    "url": "/lyrics/static/js/main.5d3ec2cf.chunk.js"
+  },
+  {
+    "revision": "d5b340048ee976f4418c",
+    "url": "/lyrics/static/js/1.d5b34004.chunk.js"
+  },
+  {
+    "revision": "5d3ec2cf17e8c07a1ffe",
+    "url": "/lyrics/static/css/main.939d1fca.chunk.css"
+  },
+  {
+    "revision": "1dd82ed10ad7343223af6422dd340a83",
+    "url": "/lyrics/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.805e9315bcd67bce13b37665316fa5fb.js"
+  "/lyrics/precache-manifest.0ead502f70f98606e24595206f98b47a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/lyrics/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
